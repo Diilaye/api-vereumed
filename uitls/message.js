5baac66a6330b879f59a4612012c167e891b1472
Diilaye <issa.k@verumko.com>
2023-03-25 15:37:34 +0000

    add login

--- uitls/message.js
+++ uitls/message.js
@@ -11,6 +11,8 @@ exports.updateObject = (object) => object  + ' modifier avec succes';
 
 exports.findObject = (object) => object  + ' trouver avec succes';
 
+exports.login = (value) => value == "1" ? "Connection réussi" :  'Identifiant   Incorrect';
+
 exports.notFindObject = (object) => object  + ' non trouveé';
 
 exports.deleteObject = (object) => object  + ' supprimer avec succes';
